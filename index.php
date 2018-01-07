b3db3dc77b102437fefc71b598a331d049dfb3ec
Larry Cromwell <lcromwellnc@gmail.com>
2018-01-07 17:33:48 -0500

    Shuffled elements on page

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
+echo "<img src = 'static/ocp.png' > <br><br>";
 echo "This is Version 2 of the App <br><br>";
-echo "The IP Address of this Pod is: ".$_SERVER['SERVER_ADDR'];
-echo "<img src = 'static/ocp.png' > <br>";
+echo "The IP Address of this Pod is: ".$_SERVER['SERVER_ADDR']"<br>";
 echo "<img src = 'static/abtesting.jpeg' > <br>";
 ?>
