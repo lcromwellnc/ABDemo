d95a23918efc77e46fc6248cbc72671e798b8732
Larry Cromwell <lcromwellnc@gmail.com>
2018-01-07 17:43:06 -0500

    More beautification

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<img src = 'static/ocp.png' > <br><br>";
+echo "<img src = 'static/ocp.png' > <br>";
 echo "This is Version 2 of the App <br><br>";
 echo "The IP Address of this Pod is: ".$_SERVER['SERVER_ADDR'];
 echo "<br> <img src = 'static/abtesting.jpeg' > <br>";
